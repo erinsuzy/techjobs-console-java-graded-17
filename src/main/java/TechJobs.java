bcbf11e8e30053e5942ff2de4baf7949e16dd9d9
Erin.Schneider <erinsuzy@gmail.com>
2023-08-22 16:20:53 -0500

    first task

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,3 +1,4 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +11,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +113,14 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
+        if (!someJobs.isEmpty()) {
             for (Map<String, String> someJob : someJobs) {
 
                 System.out.println("*****");
@@ -128,8 +129,10 @@ public class TechJobs {
                 }
                 System.out.println("*****\n");
             }
+        } else {
+            System.out.println("No Results");
         }
     }
-
+}
 
 
